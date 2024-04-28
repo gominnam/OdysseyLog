439cdef16f9824d0cf166a0d9f254b9e24836a3b
gominnam <kosok03@naver.com>
2024-04-28 19:46:36 +0900

    init odysseyLog project

---
+++ src/main/kotlin/com/example/odysseylog/OdysseyLogApplication.kt
@@ -0,0 +1,11 @@
+package com.example.odysseylog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OdysseyLogApplication
+
+fun main(args: Array<String>) {
+    runApplication<OdysseyLogApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/odysseylog/OdysseyLogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.odysseylog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OdysseyLogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
