13b7f53ec50289df4d893ac3ed1ac89b255eade9
gominnam <kosok03@naver.com>
2024-05-01 23:51:59 +0900

    fix Route datatime column saving

--- src/main/kotlin/com/example/odysseylog/OdysseyLogApplication.kt
+++ src/main/kotlin/com/example/odysseylog/OdysseyLogApplication.kt
@@ -2,8 +2,10 @@ package com.example.odysseylog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class OdysseyLogApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/odysseylog/domain/Route.kt
+++ src/main/kotlin/com/example/odysseylog/domain/Route.kt
@@ -3,9 +3,11 @@ package com.example.odysseylog.domain
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 class Route {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
