4555fa609c492ab9312eb01ab9e2d74e05cfe78f
gominnam <kosok03@naver.com>
2024-05-05 00:10:42 +0900

    add Photo entity

--- src/main/kotlin/com/example/odysseylog/domain/Photo.kt
+++ src/main/kotlin/com/example/odysseylog/domain/Photo.kt
@@ -1,4 +1,27 @@
 package com.example.odysseylog.domain
 
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
 class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    var url: String? = null
+
+    @CreatedDate
+    var createdAt: LocalDateTime? = null
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spot_id")
+    var spot: Spot? = null
 }
