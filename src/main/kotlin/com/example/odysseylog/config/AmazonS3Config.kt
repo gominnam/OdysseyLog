a41ca30c63c966f870cc9c4774b88a84a99b9895
gominnam <kosok03@naver.com>
2024-05-26 17:38:11 +0900

    change amazons3 version 1.x to 2.x

--- src/main/kotlin/com/example/odysseylog/config/AmazonS3Config.kt
+++ src/main/kotlin/com/example/odysseylog/config/AmazonS3Config.kt
@@ -1,12 +1,12 @@
 package com.example.odysseylog.config
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider
-import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
 
 @Configuration
 class AmazonS3Config {
@@ -20,11 +20,11 @@ class AmazonS3Config {
     private lateinit var region: String
 
     @Bean
-    fun amazonS3Client(): AmazonS3 {
-        val awsCredentials = BasicAWSCredentials(accessKeyId, secretAccessKey)
-        return AmazonS3ClientBuilder.standard()
-            .withRegion(region)
-            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+    fun s3Client(): S3Client {
+        val awsCredentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey)
+        return S3Client.builder()
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
             .build()
     }
 }
