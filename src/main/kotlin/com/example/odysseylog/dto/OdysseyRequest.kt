65fb02300c3aad11f807d924a86cf5ae3afd77ff
gominnam <kosok03@naver.com>
2024-06-12 21:19:50 +0900

    add dto request(Odyssey, photo)

--- src/main/kotlin/com/example/odysseylog/dto/OdysseyRequest.kt
+++ src/main/kotlin/com/example/odysseylog/dto/OdysseyRequest.kt
@@ -2,6 +2,5 @@ package com.example.odysseylog.dto
 
 data class OdysseyRequest(
     val route: RouteRequest,
-    val spots: List<SpotRequest>,
-    val photos: List<PhotoRequest>
+    val spots: List<SpotRequest>
 )
---
+++ src/main/kotlin/com/example/odysseylog/dto/PhotoRequest.kt
@@ -0,0 +1,5 @@
+package com.example.odysseylog.dto
+
+data class PhotoRequest(
+    var order: Int? = null,
+)
