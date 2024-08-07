c7d8957ae37c126cc8ec41121ef712a20cb1d1dd
gominnam <kosok03@naver.com>
2024-08-07 15:43:28 +0900

    refactor CustomException

--- src/main/kotlin/com/example/odysseylog/exception/CustomException.kt
+++ src/main/kotlin/com/example/odysseylog/exception/CustomException.kt
@@ -1,3 +1,4 @@
 package com.example.odysseylog.exception
 
-class CustomException(val errorCode: ErrorCode, vararg val args: Any) : RuntimeException(errorCode.message.format(*args))
+class CustomException(val errorCode: ErrorCode, vararg args: Any)
+    : RuntimeException(String.format(errorCode.message, *args))
